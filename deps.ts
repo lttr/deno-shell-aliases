3fbd45d4973148c2e015429a06d729598c4066a1
Lukas Trumm <lukas.trumm@gmail.com>
2024-03-29 13:39:57 +0100

    Update dependencies and fix deprecations

--- deps.ts
+++ deps.ts
@@ -1,5 +1,2 @@
-export * as path from "https://deno.land/std@0.152.0/path/mod.ts";
-export {
-  assertEquals,
-  assertRejects,
-} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+export * as path from "@std/path";
+export { assertEquals, assertRejects } from "@std/assert";
